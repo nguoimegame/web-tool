82f6f8f4455cedba8702cdb64fa81b1bdf04bf02
quangit <quang11t1@outlook.com>
2025-12-16 18:29:14 +0700

    deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://quangit.github.io',
-  base: '/web-tool'
+  base: '/web-tool',
   output: 'static',
   build: {
     format: 'file'
