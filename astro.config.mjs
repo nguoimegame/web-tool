55974521afea25024825fbd6cab23782403a8af0
Quang <quangnd@cymosoft.com>
2025-12-16 18:51:03 +0700

    deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://quangit.github.io',
-  base: '/web-tool',
   output: 'static',
   build: {
     format: 'file'
