fa36c53ef734c71b2b6653596ef556821fabf7d6
Quang <quangnd@cymosoft.com>
2025-12-21 16:52:10 +0700

    multi language

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,12 @@ export default defineConfig({
   output: 'static',
   build: {
     format: 'file'
+  },
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en", "vi", "zh", "hi", "es", "fr", "pt", "ja"],
+    routing: {
+      prefixDefaultLocale: false
+    }
   }
 });
---
+++ src/i18n/utils.ts
@@ -0,0 +1,19 @@
+import { ui, defaultLang } from './ui';
+
+export function getLangFromUrl(url: URL) {
+  const [, lang] = url.pathname.split('/');
+  if (lang in ui) return lang as keyof typeof ui;
+  return defaultLang;
+}
+
+export function useTranslations(lang: keyof typeof ui) {
+  return function t(key: keyof typeof ui[typeof defaultLang], ...args: any[]) {
+    let translation = ui[lang][key] || ui[defaultLang][key];
+    if (args.length > 0) {
+      args.forEach((arg, index) => {
+        translation = translation.replace(`{${index}}`, arg);
+      });
+    }
+    return translation;
+  }
+}
