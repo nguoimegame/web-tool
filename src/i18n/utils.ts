59d189d8fc75ace64f3b31d5e21e630f030b6c96
Quang <quangnd@cymosoft.com>
2025-12-21 18:02:03 +0700

    update multi language

--- src/i18n/utils.ts
+++ src/i18n/utils.ts
@@ -2,7 +2,8 @@ import {defaultLang, ui} from './ui';
 
 export function getLangFromUrl(url: URL) {
   const [, lang] = url.pathname.split('/');
-  if (lang in ui) return lang as keyof typeof ui;
+  const cleanLang = lang?.replace(/\.html$/, '');
+  if (cleanLang in ui) return cleanLang as keyof typeof ui;
   return defaultLang;
 }
 
